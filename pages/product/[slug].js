75849d07f9186bbbfa385a67eec418d6b8ad0c7b
igor lorris <igorlorris@gmail.com>
2022-12-07 08:28:09 +0100

    fixing a major slug bug

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -6,7 +6,7 @@ import { Product } from '../../components';
 import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
-  const { image, name, details, price } = product;
+  const { image, name, details, price } = product ;
   const [index, setIndex] = useState(0);
   const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
